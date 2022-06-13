75ec0b4731c96a8fa05d62f6a9c56155b6e1ca02
MISTER-IZZATBEK <jumaniyazovizzatbek98@gmail.com>
2022-06-13 11:32:55 +0500

    add traid

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Group;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
 
 class GroupController extends Controller
 {
+    use AbstractControllerTrait;
+
     protected $folderName='Groups';
     protected $modelClass=Group::class;
     protected $routeName='groups';
@@ -19,42 +19,4 @@ class GroupController extends Controller
             'name' => 'required',
         ];
     }
-
-    public function index()
-    {
-        $items= $this->modelClass::all();
-        return Inertia::render($this->folderName.'/Index', compact('items'));
-    }
-    public function create()
-    {
-        return Inertia::render($this->folderName.'/Create');
-    }
-    public function store(Request $request)
-    {
-        $data=$this->validate($request, $this->validationRules);
-         $this->modelClass::create($data);
-        return redirect()->route($this->routeName.'.index');
-    }
-    public function show($id)
-    {
-        $item= $this->modelClass::findOrFail($id);
-        return view($this->routeName.'.show', compact('item'));
-    }
-    public function edit($id)
-    {
-        $item= $this->modelClass::findOrFail($id);
-        return Inertia::render($this->folderName.'/Edit', compact('item'));
-    }
-    public function update(Request $request, $id)
-    {
-        $data=$this->validate($request, $this->validationRules);
-         $this->modelClass::findOrFail($id)->update($data);
-        return redirect()->route($this->routeName.'.index');
-    }
-    public function destroy($id)
-    {
-        $this->modelClass::findOrFail($id)->delete();
-        return redirect()->route($this->routeName.'.index');
-    }
-
 }
--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Teacher;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
+
 
 class TeacherController extends Controller
 {
+
+    use AbstractControllerTrait;
+
     protected $folderName='Teachers';
     protected $modelClass=Teacher::class;
     protected $routeName='teachers';
@@ -20,42 +22,5 @@ class TeacherController extends Controller
             'birthday' => 'required',
             'specialty' => 'required',
         ];
-    }
-
-    public function index()
-    {
-        $items= $this->modelClass::all();
-        return Inertia::render($this->folderName.'/Index', compact('items'));
-    }
-    public function create()
-    {
-        return Inertia::render($this->folderName.'/Create');
-    }
-    public function store(Request $request)
-    {
-        $data=$this->validate($request, $this->validationRules);
-        $this->modelClass::create($data);
-        return redirect()->route($this->routeName.'.index');
-    }
-    public function show($id)
-    {
-        $item= $this->modelClass::findOrFail($id);
-        return view($this->routeName.'.show', compact('item'));
-    }
-    public function edit($id)
-    {
-        $item= $this->modelClass::findOrFail($id);
-        return Inertia::render($this->folderName.'/Edit', compact('item'));
-    }
-    public function update(Request $request, $id)
-    {
-        $data=$this->validate($request, $this->validationRules);
-        $this->modelClass::findOrFail($id)->update($data);
-        return redirect()->route($this->routeName.'.index');
-    }
-    public function destroy($id)
-    {
-        $this->modelClass::findOrFail($id)->delete();
-        return redirect()->route($this->routeName.'.index');
     }
 }
